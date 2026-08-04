16578ddd1ed4e92b41db2499ef63a7b68a84c264
Matthew Stengel <matthewstengel69@gmail.com>
2026-08-03 20:42:01 -0500

    Compare current and suggested website metadata

---
+++ src/Ghos.Web/Data/Migrations/20260804014042_AddWebsiteHealthCurrentValues.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ghos.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWebsiteHealthCurrentValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentValue",
+                table: "WebsiteHealthIssues",
+                type: "character varying(6000)",
+                maxLength: 6000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentValue",
+                table: "WebsiteHealthIssues");
+        }
+    }
+}
