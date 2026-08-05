b0e31f9703c460457e324539911514e3ef0d1f67
Matthew Stengel <matthewstengel69@gmail.com>
2026-08-05 11:11:37 -0500

    Add safe quote deletion workflow

---
+++ src/Ghos.Web/Data/Migrations/20260805160759_AddQuoteSoftDelete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ghos.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuoteSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAtUtc",
+                table: "CustomerQuotes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedByUserId",
+                table: "CustomerQuotes",
+                type: "character varying(450)",
+                maxLength: 450,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAtUtc",
+                table: "CustomerQuotes");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedByUserId",
+                table: "CustomerQuotes");
+        }
+    }
+}
